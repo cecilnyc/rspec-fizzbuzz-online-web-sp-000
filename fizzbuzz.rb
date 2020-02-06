29150311079d829a95517f453af6f22ceaa06815
Cecilia Moody <cecimoody@icloud.com>
2020-02-06 15:42:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,10 @@
 def fizzbuzz
-  
+  if fizz_3 = fizzbuzz(3)
+    print "Fizz"
+  elseif fizz_5 = fizzbuzz(5)
+    print "Buzz"
+  elseif fizz_15 = fizzbuzz(15)
+    print "FizzBuzz"
+  else fizz_4 = fizzbuzz(4)
+    return 
 end
