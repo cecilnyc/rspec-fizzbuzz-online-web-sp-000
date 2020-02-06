cf94720651308fbd43eeb14513f3340aae69b981
Cecilia Moody <cecimoody@icloud.com>
2020-02-06 15:37:11 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,11 @@
 def fizzbuzz(int)
-  if int % 3 == "Fizz"
-  elseif int % 5 == "Buzz"
-  elseif int % 3 && 5 == "FizzBuzz"
-  elseif int % 3 || 5 == 0 
+  if int % 3 == "fizz_3"
+    puts "Fizz"
+  elseif int % 5 == "fizz_5"
+    puts "Buzz"
+  elseif int % 3 && 5 == "fizz_15"
+    puts "FizzBuzz"
+  else int % 3 || 5 == "fizz_4"
+    puts "nil" 
   
 end
