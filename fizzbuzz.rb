fa58260f9ff8445f42c8168a7e4a5fde8a4ad3bc
Cecilia Moody <cecimoody@icloud.com>
2020-02-06 15:56:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,8 +2,8 @@ def fizzbuzz(number)
   if number % 3 == 0
     "Fizz"
   elseif number % 5 == 0
-    puts "Buzz"
-  elseif fizz_15 = fizzbuzz(15)
+    "Buzz"
+  elseif number  = fizzbuzz(15)
     puts "FizzBuzz"
   else fizz_4 = fizzbuzz(4)
     return nil
